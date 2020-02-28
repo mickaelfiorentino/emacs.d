28e397c438815c621eaf0d20950c86983145f9a0
mickael@xpsus <mickael.fiorentino@polymtl.ca>
2020-02-28 15:56:05 -0500

    Removed custom theme: using prot Modus themes instead

--- themes/tango++/tango++_pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "tango++" "20190906.0" "A color theme derived from tango-plus-theme" 'nil :commit "" :authors '(("Mickael Fiorentino" . "mickael.fiorentino@mailbox.org")) :maintainer '("Mickael Fiorentino" . "mickael.fiorentino@mailbox.org") :url "")
