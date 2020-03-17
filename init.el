8169a1a5e2d05cc09fa9494006d6362cf2d06749
mickael@pcys <mickael.fiorentino@polymtl.ca>
2020-03-16 21:04:20 -0400

    Updated config to work with linux/windows

--- init.el
+++ init.el
@@ -2,13 +2,6 @@
 ;; (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (package-initialize)
 (require 'org)
+
 (setq-default user-emacs-directory "~/.emacs.d/emacs.d")
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-
-;; FONTS
-(set-face-attribute 'default nil
-                    :family  "Hack"
-                    :height  113
-                    :slant   'normal
-                    :weight  'normal
-                    :width   'normal)
